581ccdb9ffa9f6ce62d8c504f97142a136793a36
Victor Barbosa <victoraug09@gmail.com>
2023-03-28 22:59:12 -0300

    Update redax.py

--- redax.py
+++ redax.py
@@ -4,7 +4,7 @@ openai.api_key = "sua-API-key-openai"
 def gerar_texto(pergunta):
     model_engine = "text-davinci-002"
     # modelo GPT-3 que será utilizado, davinci é o mais confiável    
-    prompt = f"Por favor, escreva um texto do tipo {tipotexto} sobre o tema: {pergunta}\nResposta: "
+    prompt = f"Por favor, escreva uma redação, texto do tipo {tipotexto} sobre o tema: {pergunta}\nResposta: "
     completions = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
